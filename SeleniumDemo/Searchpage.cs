5a3fc8d8245344ebe4ca3a5d0b1b6a1dbeef7c81
mario salvatierra <msalv006@fiu.edu>
2019-08-31 19:44:49 -0400

    first commit

---
+++ SeleniumDemo/Searchpage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo
+{   [TestClass]
+    public class Searchpage
+    {
+        [TestMethod]
+        public void SearchForMyPage()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            //  IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions);
+
+            // driver.Navigate().GoToUrl("https://cvdosforfiu.azurewebsites.net");
+            driver.Navigate().GoToUrl("http://mywebappforfiu.azurewebsites.net");
+
+            Console.WriteLine("=============>>>>>>> Test completed");
+        }
+    }
+}
